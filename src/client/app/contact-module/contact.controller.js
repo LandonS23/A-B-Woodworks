b4459c635bb8d9593555814ac5d19ae01c1919a6
Landon Sherwood <landon.sherwood@hobbylobby.com>
2016-08-22 17:54:58 -0500

    progress

--- src/client/app/contact-module/contact.controller.js
+++ src/client/app/contact-module/contact.controller.js
@@ -23,19 +23,11 @@
         //        [a-zA-Z0-9]+([a-zA-Z0-9_.$\u0026-]+)*@[a-zA-Z0-9]+([a-zA-Z0-9_.$\u0026-]+)*.([a-zA-Z0-9]{2,4})
         vm.sendMail = function () {
 
-            //            var data = ({
-            //                contactName: this.contactName,
-            //                contactNumber: this.contactNumber,
-            //                contactEmail: this.contactEmail,
-            //                contactMsg: this.contactMsg
-            //            });
             console.log('Sending mail...');
-            $http.post('/api/contact', vm.data).
-            success(function (data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
 
-                console.log('Thanks for your message ' + data.contactName + ' You Rock!');
+            $http.post('/send', vm.data).
+            success(function (data, status, headers, config) {
+                console.log('Thanks for your message ' + data.contactName);
                 //                $mdToast.show(
                 //                    $mdToast.simple()
                 //                    .content('Thanks for your message ' + vm.data.contactName + ' You Rock!')
@@ -45,8 +37,6 @@
 
             }).
             error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
                 console.log('Broken');
             });
         };
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,29 @@
 
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+//var xoauth2 = require('xoauth2');
+//var generator = require('xoauth2').createXOAuth2Generator({
+//    user: 'ABWoodworksWeb@gmail.com', // Your gmail address.
+//
+//    clientId: '',
+//    clientSecret: '',
+//    refreshToken: '',
+//});
+// listen for token updates
+// you probably want to store these to a db
+//generator.on('token', function (token) {
+//    console.log('New token for %s: %s', token.user, token.accessToken);
+//});
+//var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+//login
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'ABWoodworksWeb@gmail.com',
+        pass: 'chrisaikins03'
+    }
+});
 var bodyParser = require('body-parser');
 var compress = require('compression');
 var cors = require('cors');
@@ -33,7 +56,6 @@ console.log('** DEV **');
 app.use(express.static('./src/client/'));
 app.use(express.static('./'));
 app.use(express.static('./tmp'));
-app.use('/api/contact', express.static('./src/server/api/contact/index.js'));
 app.use('/*', express.static('./src/client/index.html'));
 
 app.listen(port, function () {
@@ -43,21 +65,26 @@ app.listen(port, function () {
         '\nprocess.cwd = ' + process.cwd());
 });
 
-//var core = express.static('src/client/layout/layout.controller.js');
 
+app.post('/send', function (req, res) {
+    var data = req.body;
 
-//var sendMail = function (req, res) {
-//
-//    var data = req.body;
-//
-//    transporter.sendMail({
-//        from: data.contactEmail,
-//        to: 'landonsherwood23@gmail.com',
-//        subject: 'Message from ' + data.contactName,
-//        html: "<label>Name: </label>" + data.contactName + "<br/><label>Phone Number: </label>" + data.contactNumber + "<br/><label>Email: </label>" + data.contactEmail + "<br/><label>Message: </label>" + data.contactMsg
-//    });
-//
-//    res.json(data);
-//};
+    var mailOptions = {
+        from: '"A&amp;B Woodworks" <ABWoodworksWeb@gmail.com>',
+        to: 'landonsherwood23@gmail.com',
+        subject: 'Message from ' + data.contactName, // Subject line
+        //        text: 'Hello world ', // plaintext body
+        html: '<label>Name: </label>' + data.contactName + '<br/><label>Phone Number: </label>' + data.contactNumber + '<br/><label>Email: </label>' + data.contactEmail + '<br/><label>Message: </label>' + data.contactMsg // html body
+    };
 
-//app.use('/contact-form', express.static('.src/server/server.js')).post(sendMail);
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+        }
+        transporter.close();
+    });
+    //res.redirect('/');
+    res.json(data);
+});
